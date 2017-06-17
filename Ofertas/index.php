1ce7161bee5a01a1c3d03ccce61af221bbdeceac
Celia24Suazo <celiasuazo.24@gmail.com>
2017-06-17 15:28:24 -0600

    Ofertas y Registros
    Me faltan

---
+++ Ofertas/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ejemplo Deezer</title>
+		<meta charset="UTF8">
+
+	   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+	   <link href="css/bootstrap.min.css" rel="stylesheet">
+	   <link rel="stylesheet" href="fonts/style.css">
+
+	</head>
+	<body style="background-image:url(img/musica2.png); background-repeat:no-repeat; color:#ffffff;"><br><br>
+	<img src="img/deezer.png" width="200" height="60"/>
+	<div class="btn-group">
+	<button type="button" class="btn btn-default">Funcionalidades</button>
+    <button type="button" class="btn btn-default">Dispositivos</button>
+    <button type="button" class="btn btn-default">Ofertas</button>
+    <button type="button" class="btn btn-default">Empresa</button>    
+      </div>
+
+        <ul class="nav navbar-right top-nav">
+    	<div class="btn-group">
+      <button type="button" class="btn btn-primary">Registrase</button></div>
+      <div class="btn-group">
+      <button type="button" class="btn btn-default">Conectarse</button></div>
+      <br><br></ul><br><br>
+
+     <center><font size=60>Tu música, sin restrinciones.</font></center><br>
+     <center><font size=5>$5.99/mes<br><br>30 días gratis*</font></center>
+     <center><font size=5>Sin compromiso. Si puedes cancelar tu suscripcion cuando quieras.</font></center>
+     <br><br>
+
+     <center><button id="d" class="btn btn-lg btn-primary" type="submit">Comenzar tus 30 días de prueba.</button></center>
+     <br><br>
+
+   <center><h1><span class="icon-arrow_downward"></span></h1></center>
+
+   
+
+</div>
+	</body>
+
+
+
+</html>
