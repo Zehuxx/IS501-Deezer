fea62b68e281d020542d0d540115658fd6b3b622
gerardo1995 <gerardimar18@gmail.com>
2017-06-20 12:16:39 -0600

    nuevo
    las modificaciones a lo tuyo celia

---
+++ registro.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ejemplo Deezer</title>
+		<meta charset="UTF8">
+	
+	   <link rel="icon" href="img/fav.ico" type="image/x-icon">
+	   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+	   <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	</head><br>
+	<body style="background-image:url(img/Musica.png); color:#ffffff;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 col-md-6 col-xs-12">
+        <img src="img/goku.jpg" width="180" height="40">
+          <button type="button" style="float: right;" class="btn btn-danger">Conectarse</button>
+        </div>
+        <center><div class="col-lg-12 col-md-12 col-xs-12 " style="padding-top: 20px">
+             
+     <font size=60>Tu música. Tu flow</font><br>
+     <font size=5>Disfruta de tu música donde quieras y cuando quieras <br>
+     Regístrate gratis<br>
+   
+   <div style="width: 400px">
+   <button type="summit" class="btn btn-default btn-primary" style="width: 200px;float: left"><span style="float: left"><img src="img/goku.jpg" width="20" height="20"> Facebook</span> </button>
+   <button type="summit" class="btn btn-default btn-danger" style="width: 200px;float: left;"><span style="float: left"><img src="img/goku.jpg" width="20" height="20"> Google+<span></button><br>
+
+    
+    <input type="dirc" name="direccion" class="form-control"  placeholder="Correo electronico" >
+    <input type="dirc" name="direccion" class="form-control" placeholder="Contraseña (6 caracteres minimo)"  >
+    <input type="dirc" name="direccion" class="form-control" placeholder="Nombre de usuario">
+    <select  class="form-control" style="width: 200px;float: left;">
+                   <option value="sexo">Sexo</option> 
+                   <option value="mujer">Mujer</option>
+                   <option value="hombre">Hombre</option>
+    </select>
+    <select  class="form-control" style="width:200px;float: right;">
+                   <option value="sexo">Sexo</option> 
+                   <option value="mujer">Mujer</option>
+                   <option value="hombre">Hombre</option>
+    </select>
+   
+    </div> 
+         <button id="Reg" class="btn btn-lg btn-primary" type="submit" style="width:400px">Registrarse</button><br>
+
+              <h6>Al hacer clic en "Registrase", aceptas las condiciones</h7></br>generales de uso.</h7>
+
+
+        </div></center>
+      </div>
+    </div> 
+	</body>
+
+
+
+</html>
