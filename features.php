a13404bcefadb29a568ee5c6c51d3050bdfa89fd
josalemara <josepmaradiaga@yahoo.es>
2017-06-22 23:35:17 -0600

    feature

---
+++ features.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Ventajas</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+		<img src="img/featurefondo.jpg" class="img-responsive">
+	
+		<div class="col-md-6"></br></br></br></br></br></br>
+			<h2>Prueba la opción Flow, solo en Deezer</h2>
+			<p>El Flow te permite disfrutar de tu música favorita, de las sugerencias mas actuales y de las canciones que tanto te gustaban antes, pero que habías olvidado. Esta opción sabe lo que te gusta y lo que no, y te ofrece una amplia selección de música a tu medida. Solo tienes que pulsar el botón.
+			</p>
+			</br></br>
+			<button type="button" class="btn btn-link">Descubrir Deezer </button>
+		</div>
+		<aside class="col-md-6">
+			<img src="img/featurefondo6.jpg" class="img-responsive" width="100%">
+		</aside>
+	</div>
+		<img t src="img/featurefondo2.jpg" class="img-responsive" width="100%">
+
+	<div class="row">
+		<div class="col-md-6">
+			<img src="img/featurefondo4.png" class="img-responsive">
+
+
+		</div>
+		<aside class="col-md-6">
+				<h3 align="center">¿No hay wifi? No pasa nada.</h3>
+				<p>Con Premium+ no necesitas tener conexión a internet para poder disfrutar de tus canciones favoritas. Solo tienes que descargártelas una vez y podrás escucharlas sin conexión cuando quieras. Ya no tendrás que prescindir de tu música cuando necesites ahorrar datos o cuando estés totalmente aislado del mundo.</p>
+				<button type="button" class="btn btn-link">Descubre Premium+</button>
+		</aside>
+
+	</div>	
+	<div class="container">
+	<div class="row">
+		<img src="img/featurefondo5.jpg" class="img-responsive">
+</div>
+	</div>
+</div>
+	
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
