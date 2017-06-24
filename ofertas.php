d0a69c3bc556fc27904e56f5779966e9726f4035
Celia24Suazo <celiasuazo.24@gmail.com>
2017-06-24 17:43:46 -0600

    ofetras

--- ofertas.php
+++ ofertas.php
@@ -10,24 +10,48 @@
 	   <link rel="stylesheet" href="fonts/style.css">
 
 	</head>
-	<body style="background-image:url(img/oferta.png); background-repeat:no-repeat; color:#ffffff;"><br><br>
-	<img src="img/deezer.png" width="200" height="60"/>
-	<div class="btn-group">
-	<button type="button" class="btn btn-default">Funcionalidades</button>
-    <button type="button" class="btn btn-default">Dispositivos</button>
-    <button type="button" class="btn btn-default">Ofertas</button>
-    <button type="button" class="btn btn-default">Empresa</button>    
-      </div>
 
-        <ul class="nav navbar-right top-nav">
-    	<div class="btn-group">
-      <button type="button" class="btn btn-primary">Registrase</button></div>
-      <div class="btn-group">
-      <button type="button" class="btn btn-default">Conectarse</button></div>
-      <br><br></ul><br><br>
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    <img src="img/Mdeezer.png" width="160" height="80" class="navbar-brand">
+    </div>
+              <div class="collapse navbar-collapse" id="navbar-1">
 
-     <center><font size=60>Tu música, sin restrinciones.</font></center><br>
-     <center><font size=5>$5.99/mes<br><br>30 días gratis*</font></center>
+    <ul class="nav navbar-nav">
+      <li><a href="#"></a></li>
+     <li><a href="#"></a></li>
+      <li><a href="#">FUNCIONALIDADES</a></li>
+      <li><a href="#">DISPOSITIVO</a></li>
+      <li><a href="#">OFERTAS</a></li>
+      <li class="dropdown active">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                  Empresa <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">ACERCA DE</a></li>
+                  <li><a href="#">TRABAJOS</a></li>
+                  <li><a href="#">PRENSA</a></li>
+                  <li><a href="#">SELLOS Y ARTISTAS</a></li>
+                  <li><a href="#">AYUDA</a></li>
+                </ul>
+              </li>
+              </ul>
+
+      <div style="width: 1200px">
+      <ul class="nav navbar-nav navbar-right">
+
+      <p class="navbar-btn">
+      <a href="#"></a><button type="button" class="btn btn-primary">Registrase</button>
+      <a href="#"></a><button type="button" class="btn btn-default">Conectarse</button></p>
+      </ul></div>
+  </div>
+  </div>
+</nav>
+	<body style="background-image:url(img/ofertas.png); background-repeat:no-repeat; color:#ffffff;"><br><br><br>	
+
+     <center><strong><font size=50>TU MÚSICA, SIN RESTRINCIONES.</font></strong></center><br><br>
+     <center><strong><font size=5>$5.99 / mes<br><br>30 días gratis*</font></center></strong>
      <center><font size=5>Sin compromiso. Sí, puedes cancelar tu suscripcion cuando quieras.</font></center>
      <br><br>
 
@@ -36,8 +60,7 @@
 
    <center><h1><span class="icon-arrow_downward"></span></h1></center>
 
-   
-
+   </div>
 </div>
 	</body>
 
