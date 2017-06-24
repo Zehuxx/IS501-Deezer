32e22dc53bd559ad3363a2e023ac732096a7bce8
SelvinM <sel2230@gmail.com>
2017-06-24 13:49:39 -0600

    pagina "company" con barra superior

---
+++ company.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="vieport" content="width = device-width, initial-scale=1">
+	<title>Quienes somos</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/company.css">
+</head>
+<body style="background-image:url(img/company-background-desktop.jpg); color:#ffffff;background-repeat:no-repeat;background-size:cover;">
+	<div class="container">
+		<header>
+			<nav class="navbar navbar-inverse navbar-fixed-top" style="">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
+							<span class="sr-only">Menu</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="#" class="navbar-brand">
+							<img border="0" src="img/logoDeezer.png" width=95 height=20> 
+						</a>
+					</div>
+					<div class="collapse navbar-collapse" id="navbar-1">
+						<ul class="nav navbar-nav">
+							<li><a href="">FUNCIONALIDADES</a></li>
+							<li><a href="">DISPOSITIVOS</a></li>
+							<li><a href="">OFERTAS</a></li>
+							<li class="dropdown active">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+									Empresa <span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="#">ACERCA DE</a></li>
+									<li><a href="#">TRABAJOS</a></li>
+									<li><a href="#">PRENSA</a></li>
+									<li><a href="#">SELLOS Y ARTISTAS</a></li>
+									<li><a href="#">AYUDA</a></li>
+								</ul>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<p class="navbar-btn">
+                   					<a href="#" class="btn btn-primary">Registrarse</a>
+               					</p>
+							</li>
+							<li>
+								<p class="navbar-btn">
+									<a href="#" class="btn btn-default">Conectarse</a>
+								</p>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+			</nav>
+			<center>
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 "> 
+	        			<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	        			<font size=100>DONDE VIVE LA MÚSICA</font>
+	    			</div>
+				</div>
+	    	</center>
+		</header>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
